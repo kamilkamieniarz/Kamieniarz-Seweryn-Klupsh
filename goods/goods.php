3ac1a3062cda75ca8ea3c39f1aa03abea0b70b58
kamilkamieniarz <kamil.kamieniarz98@gmail.com>
2021-04-06 22:07:34 +0200

    1.1.1
    -poprawka goods.php

--- goods/goods.php
+++ goods/goods.php
@@ -27,9 +27,9 @@
 		$ile = mysqli_num_rows($records);  //ilosc wszystkich rekordow (nie stron !!)
 		$na_strone = 6; //tu podajesz ile rekordow na stronie max.
 		$stron = ceil ($ile / $na_strone);   //tutaj masz ilosc stron zaokraglanych w gore
+		if (!isset($_GET['strona'])) $strona = 1; else $strona = (int)$_GET['strona'];
 		$sql = mysqli_query($conn,"SELECT  goods.id, goods.name, goods.unit_price, goods.unit_of_measure ,producers.Name FROM goods LEFT OUTER JOIN producers ON producers.ID = goods.id_producer LIMIT ".(($strona-1)*$na_strone).','.$na_strone);	// tak odczytujesz
 		echo 'Strona:';
-		if (!isset($_GET['strona'])) $strona = 1; else $strona = (int)$_GET['strona'];
 		echo '<a href="?strona=1"> 1</a> ';
 		for ($i = 1; $i < $stron; $i++) echo '	<a href="?strona='.($i+1).'"> '.($i+1).'</a> ';  //tak wyswietlasz numery;
 		echo '<table class="table table-striped table-hover">
